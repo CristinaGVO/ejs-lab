9e435f4071bccce31a4e69f1ea5a3a3798310c4b
CristinaGVO <vargasosoriocristina@gmail.com>
2025-12-03 13:52:26 -0800

    Add update to the code

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const path = require ('path'); // rutas de carpetas
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
 const RESTAURANT = {
     name: 'The Green Byte Bistro',
